df85b2dbb98e286f20358b96448c0539ce8b410e
kod-python <kelvinlik0245@gmail.com>
2023-12-02 14:11:41 +0000

    add one

--- j.js
+++ j.js
@@ -17,24 +17,48 @@ var myVar = setInterval(myTimer, 1000);
 
 
 
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     direction: 'horizontal',
-//     loop: true,
-  
-//     // // If we need pagination
-//     // pagination: {
-//     //   el: '.swiper-pagination',
-//     // },
-  
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
+// better swiper starts here
+
+// let menu = document.querySelector('#menu-btn');
+// let navbar = document.querySelector('.navbar');
+
+// menu.onclick = () => {
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+// };
+
+// window.onscroll = () => {
+//     menu.classList.remove('fa-times');
+//     navbar.classList.remove('active');
+// };
+
+// document.querySelectorAll('.image-slider img').forEach(images => {
+//     images.onclick = () => {
+//         var src = images.getAttribute('src');
+//         document.querySelector('.main-home-image').src = src;
+//     };
+// });
+
+// var swiper = new Swiper(".review-slider", {
+//     spaceBetween: 20,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
 //     },
-  
-//     // // And if we need scrollbar
-//     // scrollbar: {
-//     //   el: '.swiper-scrollbar',
-//     // },
-//   });
+//     loop: true,
+//     grabCursor: true,
+//     autoplay: {
+//         delay: 7500,
+//         disableOnInteraction: false,
+//     },
+//     breakpoints: {
+//         0: {
+//             slidesPerView: 1
+//         },
+//         768: {
+//             slidesPerView: 2
+//         }
+//     },
+// });
+
+// better swipper ends here
